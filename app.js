6921cf87c1ee6610599b23405994a4d2c274cb34
TulsiHudka <tulsi.hudka@aspiresoftserv.in>
2023-04-05 16:55:14 +0530

    crud

--- app.js
+++ app.js
@@ -5,9 +5,9 @@ require("./src/db/conn");
 // const port = process.env.PORT || 5000;
 
 
-// const cors = require('cors')
+const cors = require('cors')
 
-// app.use(cors());
+app.use(cors());
 
 app.use(express.json());
 
@@ -15,13 +15,63 @@ app.get("/blogs", async (req, res) => {
   try {
     const getBlogs = await Blog.find({});
     console.log(getBlogs);
-    res.json({ data: getBlogs });
+    res.json(getBlogs);
   } catch (e) {
     res.status(400).send(e);
   }
 })
 
+//for individual req
+app.get("/blogs/:id", async(req, res) => {
+  try{
+      const _id =  req.params.id;
+      const getBlog = await Blog.findById(_id);
+      res.send(getBlog);
+  }catch(e){
+      res.status(400).send(e);
+  }
+})
 
-app.listen(5000, () => {
+//for addBlog 
+
+app.post("/addBlog", async(req, res) => {
+  try{
+      const addingBlogs = new Blog(req.body);
+      const insertBlogs = await addingBlogs.save();
+      console.log(insertBlogs);
+      res.status(201).send(insertBlogs);
+  }catch(e){
+      res.status(400).send(e);
+  }
+})
+
+
+//for deleting blog
+
+app.delete("/blogs/:id", async(req, res) => {
+  try{
+      const _id =  req.params.id;
+      const deleteBlog = await Blog.findByIdAndDelete(req.params.id);
+      res.send(deleteBlog);
+  }catch(e){
+      res.status(500).send(e);
+  }
+})
+
+//for editing blog
+
+app.put("/edit/:id", async(req, res) => {
+  try{
+      const _id =  req.params.id;
+      const editBlog = await Blog.findByIdAndUpdate(_id, req.body, {
+          new:true
+      });
+      res.send(editBlog);
+  }catch(e){
+      res.status(500).send(e);
+  }
+})
+
+app.listen(8000, () => {
   console.log(`server is running at `);
 })
--- src/db/conn.js
+++ src/db/conn.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose");
 
 mongoose.connect("mongodb+srv://tulsihudka:Tuls!4212@cluster0.epjkykc.mongodb.net/test")
-  .then(() => {
-    console.log("Connection Succesful");
-  }).catch(() => {
-    console.log("No Connection");
-  });
+    .then(() => {
+    console.log("connection sucessful");
+    }).catch(() => {
+    console.log("no connection");
+})
