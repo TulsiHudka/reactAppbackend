b3529f527de7da896bd5985ca0b2f1cf3069066a
TulsiHudka <tulsi.hudka@aspiresoftserv.in>
2023-07-17 16:00:25 +0530

    final changes

--- app.js
+++ app.js
@@ -10,18 +10,36 @@ const userRouter = require("./routers/userRoutes")
 const passwordRouter = require("./routers/passwordRoutes")
 require("./src/db/conn");
 const cors = require('cors')
+const eurekaHelper = require('./eureka-helper');
+// const express = require('express');
+// const app = express();
+// const eurekaHelper = require('./eureka-helper');
+const PORT = 9000;
+
+// app.listen(PORT, () => {
+//   console.log("user-service on 6000");
+// })
+
+// app.get('/', (req, res) => {
+//   res.json("I am user-service")
+// })
+eurekaHelper.registerWithEureka('user-management', PORT);
+
+
+// eurekaHelper.registerWithEureka('user-service', PORT);
+
+
 
 app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: false }));
+// app.use("/")
 app.use("/uploads", express.static("uploads"));
 app.use("/blogs", blogRouter)
 app.use("/users", userRouter)
 app.use("/password", passwordRouter)
 
-app.listen(8000, () => {
+app.listen(5000, () => {
   console.log(`server is running at `);
 })
-
-
